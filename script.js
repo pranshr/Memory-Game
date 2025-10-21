dfcaeda2f7c0d7829e33f9e2c37d62fc646411e2
Pranjal Sharma <pranshr005@gmail.com>
2025-10-21 19:37:28 +0530

    Cards are now generated through JS, though click functionality is gone for now.

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const card1 = document.getElementById('card1');
-card1.addEventListener('click', function() {
+// Testing card flipping and unflipping
+/*
+const card = document.getElementById('card');
+
+card.addEventListener('click', function() {
     console.log("Card Clicked!");
-    if (card1.classList.contains('flipped')) {
-        card1.classList.remove('flipped');
+    if (!card.classList.contains('flipped')) {
+        card.classList.add('flipped');
     } else {
-        card1.classList.add('flipped');
+        card.classList.remove('flipped');
     }
 });
+*/
+
+function buildCard() {
+    const cardWrapper = document.createElement('div');
+    const card = document.createElement('div');
+    const front = document.createElement('div');
+    const back = document.createElement('div');
+
+    cardWrapper.classList.add('card-wrapper')
+    card.classList.add('card');
+    front.classList.add('front');
+    back.classList.add('back');
+
+    cardWrapper.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
+    console.log("Card Made!");
+
+    return cardWrapper;
+}
+
+// Adding more cards to the board
+const gameArea = document.getElementById('game-area');
+for (let i=0; i < 8; i++) {
+    gameArea.appendChild(buildCard());
+
+}
